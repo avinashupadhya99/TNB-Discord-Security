1e3cf2e1c3d24ce0e006b81614c91d9245e43274
Avinash Upadhyaya <avinashupadhya99@gmail.com>
2021-07-06 15:31:47 +0530

    feat: create new account and send coins from exposed account

--- src/account.ts
+++ src/account.ts
@@ -1,9 +1,10 @@
-import { Account, PrimaryValidator, ConfirmationValidator } from "thenewboston";
+import { Account, AccountPaymentHandler, PrimaryValidator, ConfirmationValidator } from "thenewboston";
 
 const primaryValidator = new PrimaryValidator(process.env.PRIMARY_VALIDATOR || 'http://54.219.183.128');
 const confirmationValidator = new ConfirmationValidator(process.env.CONFIRMATION_VALIDATOR || 'http://3.101.33.24');
+const bankUrl = process.env.BANK_URL || 'http://13.233.77.254';
 
-export const checkSigningKey = async (key: string) => {
+export const checkSigningKey = async (key: string): Promise<string> => {
     // Create account with the "signing key"
     const account: Account = new Account(key);
 
@@ -12,11 +13,37 @@ export const checkSigningKey = async (key: string) => {
     if(balance) {
         const requiredAmount: number = Number(process.env.TRANSACTION_FEES) + Number(process.env.LOWEST_UNIT_TNBC);
         if(balance >= requiredAmount) {
-            transferCoins(account);
+            const newSigningKey: string = await transferCoins(account, balance);
+            return new Promise<string>((resolve, reject) => {
+                resolve(newSigningKey);
+            });
         }
     }
+    // Reject if balance is not sufficient
+    return new Promise<string>((resolve, reject) => {
+        reject();
+    });
 }
 
-const transferCoins = (account: Account) => {
+const transferCoins = async(account: Account, balance: number): Promise<string> => {
+    const paymentHandlerOptions = {
+        account: account,
+        bankUrl: bankUrl,
+    };
 
+    const paymentHandler = new AccountPaymentHandler(paymentHandlerOptions);
+
+    await paymentHandler.init();
+
+    const newAccount: Account = new Account();
+    console.log(newAccount.accountNumberHex);
+    console.log(newAccount.signingKeyHex);
+
+    const amount: number = balance - Number(process.env.TRANSACTION_FEES);
+
+    await paymentHandler.sendCoins(newAccount, amount, "Transferring coins due to exposed signing key on Discord");
+    
+    return new Promise<string>((resolve, reject) => {
+        resolve(newAccount.signingKeyHex);
+    });
 }
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { Client, Guild, MessageEmbed } from 'discord.js';
+import { Client, Guild } from 'discord.js';
 
 import * as dotenv from 'dotenv';
 import { checkSigningKey } from './account';
@@ -11,11 +11,39 @@ client.on('ready', () => {
     client.guilds.cache.map((guild: Guild) => console.log(guild.id));
 });
 
-client.on('message', message => {
+client.on('message', async (message) => {
+    console.log(message.content);
     const pattern = /[a-zA-Z0-9]{64}/g;
     let match = pattern.exec(message.content);
     while(match) {
-        checkSigningKey(match[0]);
+        console.log(match[0]);
+        try{
+            const newSigningKey = await checkSigningKey(match[0]);
+            
+            if(newSigningKey) {
+                let reply: string = `
+                **Signing Key exposed**
+
+                A certain signing key was exposed in this message. **One should not be sharing their signing key with anyone**. Please **check your DM** for further details`;
+                message.author.send(`
+                Hello,
+                It looks like you accidentally pasted your TNB Wallet signing key on a Discord server. We have transferred all coins from the exposed account to a new account. Here's the signing key for your new account - \`${newSigningKey}\`
+                Checkout https://thenewboston.com/wallet/recover-an-account for detailed steps on recovering your account using the signing key.
+                `).catch(async err => {
+                    console.log(err);
+                    reply = `**Signing Key exposed**
+                    
+                    A certain signing key was exposed in this message. **One should not be sharing their signing key with anyone**. We could not reach you through your DM for providing your new signing key. Please enable DMs and use \`/recover_compromised_wallets\` to recover your account`
+                    // TODO: Store in database
+                });
+                message.reply(reply);
+                // Stop checking if a signing key is already found
+                break;
+            }
+        } catch(exception) {
+            console.error(exception);
+            // Do nothing
+        }
         pattern.lastIndex = match.index + 1;
         match = pattern.exec(message.content);
     }
