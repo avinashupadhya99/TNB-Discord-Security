dd9da3d7f3bdf5e85e06c925d5f9078a32c86397
Avinash Upadhyaya <avinashupadhya99@gmail.com>
2021-07-25 19:50:41 +0530

    chore: refactor callback for command to the command file

--- src/commands/recover_compromised_wallets.ts
+++ src/commands/recover_compromised_wallets.ts
@@ -1,8 +1,61 @@
+import { Client } from "discord.js";
+import { Key } from '../data/entities/key.entity';
+import { KeysRepository } from '../database';
+
 module.exports.schema = {
     "name": "recover_compromised_wallets",
     "description": "Recover your compromised wallet"
 };
 
-module.exports.callback = async (userId: string) => {
+module.exports.callback = async (client: Client, interaction: any, ) => {
+    const userId: string = interaction?.user?.id ?? interaction?.member?.user?.id;
+    const channel = await client.channels.cache.get(interaction.channel_id);
+        if(!channel) {
+            console.log(interaction);
+            client.api.interactions(interaction.id, interaction.token).callback.post({data: {
+                type: 4,
+                data: {
+                    content: 'Something went wrong! Please try again later'
+                }
+            }});
+        }
+        if(channel && channel.type === 'dm') {
+            const keysRepository: KeysRepository = new KeysRepository();
+            const keys: Key[] = await keysRepository.findByUserID(userId);
+            if(keys.length > 0) {
+                // Join all signing keys to a string separated by commas if there are multiple
+                const keyString: string = keys.map(key => `\`${key.signingkey}\``).join(', ');
+                try{
+                    await client.api.interactions(interaction.id, interaction.token).callback.post({data: {
+                        type: 4,
+                        data: {
+                            content: 
+`Hello,
+It looks like you accidentally pasted your TNB Wallet signing key(s) on a Discord server. We have transferred all coins from the exposed account(s) to new account(s). Here's the signing key(s) for your new account(s) - ${keyString}
+Checkout https://thenewboston.com/wallet/recover-an-account for detailed steps on recovering your account using the signing key(s).`
+                        }
+                    }});
+                    // Delete once DM is sent
+                    keysRepository.deleteByUserID(userId);
+                } catch (exception) {
+                    // DM failed
+                    console.error(exception);
+                }
 
+            } else {
+                client.api.interactions(interaction.id, interaction.token).callback.post({data: {
+                    type: 4,
+                    data: {
+                        content: 'No keys found for this user'
+                    }
+                }});
+            }
+        } else {
+            client.api.interactions(interaction.id, interaction.token).callback.post({data: {
+                type: 4,
+                data: {
+                    content: 'Use the command in DM to retreive your new signing key in case your wallet was compromised'
+                }
+            }});
+        }
 };
--- src/index.ts
+++ src/index.ts
@@ -67,58 +67,9 @@ A certain signing key was exposed in this message. **One should not be sharing t
 
 client.ws.on('INTERACTION_CREATE', async interaction => {
     const command: string = interaction.data.name.toLowerCase();
-    const userId: string = interaction?.user?.id ?? interaction?.member?.user?.id;
 
     if(command === 'recover_compromised_wallets') {
-        const channel = await client.channels.cache.get(interaction.channel_id);
-        if(!channel) {
-            console.log(interaction);
-            client.api.interactions(interaction.id, interaction.token).callback.post({data: {
-                type: 4,
-                data: {
-                    content: 'Something went wrong! Please try again later'
-                }
-            }});
-        }
-        if(channel && channel.type === 'dm') {
-            const keysRepository: KeysRepository = new KeysRepository();
-            const keys: Key[] = await keysRepository.findByUserID(userId);
-            if(keys.length > 0) {
-                // Join all signing keys to a string separated by commas if there are multiple
-                const keyString: string = keys.map(key => `\`${key.signingkey}\``).join(', ');
-                try{
-                    await client.api.interactions(interaction.id, interaction.token).callback.post({data: {
-                        type: 4,
-                        data: {
-                            content: 
-`Hello,
-It looks like you accidentally pasted your TNB Wallet signing key(s) on a Discord server. We have transferred all coins from the exposed account(s) to new account(s). Here's the signing key(s) for your new account(s) - ${keyString}
-Checkout https://thenewboston.com/wallet/recover-an-account for detailed steps on recovering your account using the signing key(s).`
-                        }
-                    }});
-                    // Delete once DM is sent
-                    keysRepository.deleteByUserID(userId);
-                } catch (exception) {
-                    // DM failed
-                    console.error(exception);
-                }
-
-            } else {
-                client.api.interactions(interaction.id, interaction.token).callback.post({data: {
-                    type: 4,
-                    data: {
-                        content: 'No keys found for this user'
-                    }
-                }});
-            }
-        } else {
-            client.api.interactions(interaction.id, interaction.token).callback.post({data: {
-                type: 4,
-                data: {
-                    content: 'Use the command in DM to retreive your new signing key in case your wallet was compromised'
-                }
-            }});
-        }
+        require('./commands/recover_compromised_wallets').callback(client, interaction);
     }
 })
 
